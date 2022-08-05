6b9dfddd24760f21aa38e66ddbebb72c61163397
Joyonto Karmakar <joyonto1997@gmail.com>
2022-08-05 16:45:45 +0600

    added patternPrint-w code

---
+++ patternPrint-W.js
@@ -0,0 +1,20 @@
+let n = 5;
+let string = "";
+let i, j, counter = parseInt(n / 2);
+for (i = 0; i < n; i++) {
+  string += "*";
+  for (j = 0; j <= n; j++) {
+    if (j == n){
+      string += "*";
+    } else if((i >= parseInt(n / 2)) && (j == counter || j == n - counter - 1)){
+      string += "*";
+    } else{
+     string +=" ";
+    }
+  }
+  if (i >= parseInt(n / 2)) {
+    counter++;
+  }
+  string += "\n";
+}
+console.log(string);
