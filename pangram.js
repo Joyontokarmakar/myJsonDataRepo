32b2d07d6766fd415c734a725f0440625d83db13
Joyonto Karmakar <joyonto1997@gmail.com>
2022-08-11 23:47:36 +0600

    string manipulation for object is going on

--- pangram.js
+++ pangram.js
@@ -1,13 +1,15 @@
 const isPangram = (str) => {
      str = str.toLowerCase();
-     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+     const object = {};
      for (let i = 0; i < str.length; i++) {
+          object[alphabet[i]] = str[i];
           const index = alphabet.indexOf(str[i]);
           if (index !== -1) {
                alphabet.splice(index, 1);
           };
      };
-
+     console.log(object)
      if(alphabet.length){
           return 'Not Pangram'
      } else {
