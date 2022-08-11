356913b09b9166860f1248224f6d6255ec7087e2
Joyonto Karmakar <joyonto1997@gmail.com>
2022-08-11 23:58:08 +0600

    string manipulation for object is on going

--- pangram.js
+++ pangram.js
@@ -3,18 +3,19 @@ const isPangram = (str) => {
      const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
      const object = {};
      for (let i = 0; i < str.length; i++) {
-          object[alphabet[i]] = str[i];
+          // object[alphabet[i]] = (str.match(alphabet[i]) || []).length;
+          object[alphabet[i]] = str.split(alphabet[i]).length - 1;
           const index = alphabet.indexOf(str[i]);
           if (index !== -1) {
                alphabet.splice(index, 1);
           };
      };
-     console.log(object)
+     console.log(JSON.stringify(object,undefined,4))
      if(alphabet.length){
           return 'Not Pangram'
      } else {
           return 'Pangram'
      }
 };
-console.log(isPangram('The quick brown fox jumps over the lazy dog.'));
-// console.log(isPangram('Joyonto karmakar'));
+// console.log(isPangram('The quick brown fox jumps over the lazy dog.'));
+console.log(isPangram('Joyonto karmakar'));
