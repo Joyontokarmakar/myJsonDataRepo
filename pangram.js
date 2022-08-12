f74e66964df7d53ab6b468326bff797ed37b2520
Joyonto Karmakar <joyonto1997@gmail.com>
2022-08-12 20:11:07 +0600

    pangram check and character count in object done

--- pangram.js
+++ pangram.js
@@ -1,21 +1,24 @@
-const isPangram = (str) => {
-     str = str.toLowerCase();
+const checkPangram = (input) => {
+     stringInput = input.toLowerCase();
      const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
      const object = {};
-     for (let i = 0; i < str.length; i++) {
-          // object[alphabet[i]] = (str.match(alphabet[i]) || []).length;
-          object[alphabet[i]] = str.split(alphabet[i]).length - 1;
-          const index = alphabet.indexOf(str[i]);
+     for (let i = 0; i < alphabet.length; i++) {
+          if(stringInput.includes(alphabet[i])){
+               object[alphabet[i]] = stringInput.split(alphabet[i]).length-1
+          }
+     }
+     for (let i = 0; i < stringInput.length; i++) {
+          const index = alphabet.indexOf(stringInput[i]);
           if (index !== -1) {
                alphabet.splice(index, 1);
           };
      };
-     console.log(JSON.stringify(object,undefined,4))
+     console.log("Object: ",JSON.stringify(object, undefined, 4))
      if(alphabet.length){
-          return 'Not Pangram'
+          return 'Not Pangram';
      } else {
-          return 'Pangram'
+          return 'Pangram';
      }
 };
-// console.log(isPangram('The quick brown fox jumps over the lazy dog.'));
-console.log(isPangram('Joyonto karmakar'));
+// console.log(checkPangram('The quick brown fox jumps over the lazy dog.'));
+console.log(checkPangram('Joyonto karmakar'));
