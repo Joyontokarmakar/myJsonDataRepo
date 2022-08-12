8803d3b60f44f103877b105cf62a905bf69abb8a
Joyonto Karmakar <joyonto1997@gmail.com>
2022-08-12 20:26:37 +0600

    pangram check done by .every function

--- pangram.js
+++ pangram.js
@@ -1,23 +1,17 @@
 const checkPangram = (input) => {
      stringInput = input.toLowerCase();
-     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
      const object = {};
+     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
      for (let i = 0; i < alphabet.length; i++) {
           if(stringInput.includes(alphabet[i])){
                object[alphabet[i]] = stringInput.split(alphabet[i]).length-1
           }
      }
-     for (let i = 0; i < stringInput.length; i++) {
-          const index = alphabet.indexOf(stringInput[i]);
-          if (index !== -1) {
-               alphabet.splice(index, 1);
-          };
-     };
      console.log("Object: ",JSON.stringify(object, undefined, 4))
-     if(alphabet.length){
-          return 'Not Pangram';
+     if(alphabet.every(char => stringInput.includes(char))){
+          return 'pangram'
      } else {
-          return 'Pangram';
+          return 'Not pangram'
      }
 };
 // console.log(checkPangram('The quick brown fox jumps over the lazy dog.'));
