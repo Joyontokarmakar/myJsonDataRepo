b4c14c7374274ed9721ade618fc98d219265294b
Joyonto Karmakar <joyonto1997@gmail.com>
2022-08-13 21:46:45 +0600

    time complexity and complexity findout

--- pangram.js
+++ pangram.js
@@ -1,5 +1,5 @@
 const checkPangram = (input) => {
-     stringInput = input.toLowerCase();
+     const stringInput = input.toLowerCase();
      const object = {};
      const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
      for (let i = 0; i < alphabet.length; i++) {
@@ -17,3 +17,6 @@ const checkPangram = (input) => {
 };
 // checkPangram('The quick brown fox jumps over the lazy dog.');
 checkPangram('Joyonto karmakar');
+
+// Time Complexity : 2n
+// Space Complexity : 4(function)+4(1st const)+4(2nd const)+4(3rd const)+4n(for loop)+ 4n = 16+8n
