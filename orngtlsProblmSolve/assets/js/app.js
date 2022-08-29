31cbb040e37c3ef2f2e2dfece3bf7b63cac996a4
Joyonto Karmakar <joyonto1997@gmail.com>
2022-08-29 15:07:52 +0600

    profile info section implemented

--- orngtlsProblmSolve/assets/js/app.js
+++ orngtlsProblmSolve/assets/js/app.js
@@ -4,6 +4,31 @@ dropdown.addEventListener('click', (e) => {
   if (dropdown.classList.contains('closed')) {
     dropdown.classList.remove('closed')
   } else {
-  dropdown.classList.add('closed')    
+    dropdown.classList.add('closed')
   }
 })
+
+
+// tab
+window.addEventListener("load", function () {
+  var myTabs = document.querySelectorAll("ul.tabName > li");
+  function myTabClicks(tabClickEvent) {
+    for (var i = 0; i < myTabs.length; i++) {
+      myTabs[i].classList.remove("active");
+    }
+    var clickedTab = tabClickEvent.currentTarget;
+    clickedTab.classList.add("active");
+    tabClickEvent.preventDefault();
+    var myContentPanes = document.querySelectorAll(".tabData");
+    for (i = 0; i < myContentPanes.length; i++) {
+      myContentPanes[i].classList.remove("active");
+    }
+    var anchorReference = tabClickEvent.target;
+    var activePaneId = anchorReference.getAttribute("href");
+    var activePane = document.querySelector(activePaneId);
+    activePane.classList.add("active");
+  }
+  for (i = 0; i < myTabs.length; i++) {
+    myTabs[i].addEventListener("click", myTabClicks);
+  }
+});
